248f8a76cc40c995b17c9c978f603abc4d6215c4
haemiii <73984822+haemiii@users.noreply.github.com>
2023-05-07 20:34:04 +0900

    id/pw + JWT

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,8 +2,10 @@ import { connectDB } from "@/util/database";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-//기본적으로 jst 사용됨!
+//기본적으로 jwt 사용됨!
 
 export const authOptions = {
   providers: [
@@ -11,7 +13,63 @@ export const authOptions = {
       clientId: "ab9bf5f04e1ac661cf0f",
       clientSecret: "60ff10ecf82e467a023544d00165776fabe7f3cf",
     }),
+
+    CredentialsProvider({
+      // -> jwt사용!!!
+      //1. 로그인페이지 폼 자동생성해주는 코드
+      name: "credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+
+      //2. 로그인요청시 실행되는코드
+      //직접 DB에서 아이디,비번 비교하고
+      //아이디,비번 맞으면 return 결과, 틀리면 return null 해야함
+      async authorize(credentials) {
+        let db = (await connectDB).db("notice");
+        let user = await db
+          .collection("user_cred")
+          .findOne({ email: credentials.email });
+        if (!user) {
+          console.log("해당 이메일은 없음");
+          return null;
+        }
+        const pwcheck = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+        if (!pwcheck) {
+          console.log("비번틀림");
+          return null;
+        }
+        return user;
+      },
+    }),
   ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, //30일
+  },
+
+  callbacks: {
+    //4. jwt 만들 때 실행되는 코드
+    //user변수는 DB의 유저정보담겨있고 token.user에 뭐 저장하면 jwt에 들어갑니다.
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = {};
+        token.user.name = user.name;
+        token.user.email = user.email;
+      }
+      return token;
+    },
+    //5. 유저 세션이 조회될 때 마다 실행되는 코드
+    session: async ({ session, token }) => {
+      session.user = token.user;
+      return session;
+    },
+  },
+
   secret: "asiefkdmvlj5k46", //jwt생성시 쓰는 암호
   adapter: MongoDBAdapter(connectDB), // session 기능 사용하기 위함!
   //redisdb도 사용할 수 있음! => ram에다가 저장
